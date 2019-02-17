78de68c3cff1f73b6e4c7ad64f817e01a9aebac6
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 18:50:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,101 @@
+class Dog
+attr_accessor :name, :breed, :id
+def initialize(name:, breed:, id:nil)
+  @name= name
+  @breed= breed
+  @id= id
+end
+
+def self.create_table
+  sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+  SQL
+  DB[:conn].execute(sql)
+  end
+def self.drop_table
+  sql =  <<-SQL
+  DROP TABLE dogs;
+  SQL
+  DB[:conn].execute(sql)
+end
+
+def save
+ if self.id
+   self.update
+  else
+  sql =  <<-SQL
+  INSERT INTO dogs (name,breed)values (?,?)
+  SQL
+  DB[:conn].execute(sql,self.name,self.breed)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+ end
+ self
+end
+
+def self.create(name:, breed:)
+file = Dog.new(name:name, breed:breed)
+file.save
+file
+end
+
+def self.new_from_db(row)
+Dog.new(id:row[0],name:row[1],breed:row[2])
+end
+
+  def self.find_by_name(name)
+    sql =  <<-SQL
+  SELECT *FROM dogs WHERE name  = ?;
+    SQL
+    row=DB[:conn].execute(sql,name)
+  Dog.new_from_db(row[0])
+  end
+def self.find_or_create_by(name:,breed:)
+  sql =  <<-SQL
+  SELECT *FROM dogs WHERE name  = ? and breed =?;
+ SQL
+ file=DB[:conn].execute(sql,name,breed)
+  if !file.empty?
+    row=file[0]
+    file=Dog.new(id:row[0],name:row[1],breed:row[2])
+  else
+    file= self.create(name:name, breed:breed)
+  end
+file
+end
+
+
+
+
+  def self.find_by_id(id)
+    sql =  <<-SQL
+  SELECT *FROM dogs WHERE id  = ? LIMIT 1;
+    SQL
+    row=DB[:conn].execute(sql,id)
+  Dog.new_from_db(row[0])
+  end
+
+    def update
+      sql =  <<-SQL
+    UPDATE dogs SET name = ?, breed = ? Where  id =?
+    SQL
+      DB[:conn].execute(sql,self.name,self.breed,self.id)
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
