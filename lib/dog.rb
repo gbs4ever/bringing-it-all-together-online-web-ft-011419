a65ef68e42055cd3f88862ef78965cdac535edf4
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 15:46:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,16 @@ def initialize(name:,breed:,id:nil)
 end
 
 def self.create_table
+  sql =  <<-SQL
+    CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      breed TEXT
+      )
+  SQL
+  DB[:conn].execute(sql)
+  end
+
 
 end
 
