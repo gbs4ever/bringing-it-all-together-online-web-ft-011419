5d244054e91555cfcb690a520d0f00e7b9282bc0
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-17 16:08:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,6 +28,8 @@ def save
   INSERT INTO dogs (name,breed)values (?,?)
   SQL
   DB[:conn].execute(sql,self.name,self.breed)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  
 end
 
 
